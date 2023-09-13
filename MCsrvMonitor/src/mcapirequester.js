fbd07f97f6e90ec7ed9513a75021413bcc522bae
KustelR <dmitriysstudios2@gmail.com>
2023-09-13 14:08:34 +0400

    Update TODO

--- MCsrvMonitor/src/mcapirequester.js
+++ MCsrvMonitor/src/mcapirequester.js
@@ -2,9 +2,7 @@ module.exports = function getData(url, port = 25565) {
     return fetch('https://mcapi.us/server/status?ip=' + url + '&port=' + port)
         .then(response => response.json())
         .then(data => {
-            if (data.error) {
-                throw new Error(data.error);
-            }
+            if (data.error) throw new Error(data.error);
             return data;
         });
 }
