d8fee29143d6d9afd627f29038839da6dfe47ddd
KustelR <138294354+KustelR@users.noreply.github.com>
2023-09-13 16:56:37 +0400

    Update node.js.yml

--- MCsrvMonitor/src/mcapirequester.js
+++ MCsrvMonitor/src/mcapirequester.js
@@ -1,6 +1,3 @@
-const fetch = require('node-fetch');
-
-
 module.exports = function getData(url, port = 25565) {
     return fetch('https://mcapi.us/server/status?ip=' + url + '&port=' + port)
         .then(response => response.json())
