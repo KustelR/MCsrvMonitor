9327e11e908d87a2325398a7f9c99e7a95f0e7ae
KustelR <dmitriysstudios2@gmail.com>
2023-09-17 12:37:07 +0400

    Update writer

--- MCsrvMonitor/src/writer.js
+++ MCsrvMonitor/src/writer.js
@@ -10,8 +10,8 @@ async function writeSmth(path, text) {
 }
 
 
-async function createDirectory(path) {
-    await fs.mkdirSync(path, (err) => {
+function createDirectory(path) {
+    fs.mkdirSync(path, (err) => {
         if (err) throw err;
     })
 }
@@ -22,33 +22,41 @@ function isDirExists(path) {
 }
 
 
+function createDirIfNotExists(path) {
+    if (!isDirExists(path)) {
+        createDirectory(path);
+    }
+}
+
+
+function generatePath(path, ip) {
+    const now = new Date();
+    const dateName = `${now.getFullYear()}y${now.getMonth() + 1}m${now.getDate()}d`
+
+    let report_path;
+    let server_folder_path;
+
+    if (!ip) {
+        report_path = `${path}/${dateName}.txt`
+    }
+    else {
+        report_path = `${path}${ip.replaceAll('.', '')}/${dateName}.txt`
+        server_folder_path = `${path}/${ip.replaceAll('.', '')}`;
+    }
+    return { report_path, server_folder_path }
+}
+
+
 module.exports = {
-    async writeNewReport(online, ip = undefined, path = undefined) {
-        const now = new Date();
+    async writeNewReport(online, ip = undefined, path = STANDARD_PATH) {
         const text = `${online} players\n${now}`;
-        const dateName = `${now.getFullYear()}y${now.getMonth() + 1}m${now.getDate()}d`
 
-        let report_path;
-        let server_folder_path;
-
-        if (!path) {
-            path = STANDARD_PATH;
-        }
-        if (!ip) {
-            report_path = `${path}/${dateName}.txt`
-        }
-        else {
-            report_path = `${path}/${ip.replaceAll('.', '')}/${dateName}.txt`
-            server_folder_path = `${path}/${ip.replaceAll('.', '')}`;
-        }
+        paths = generatePath(path, ip);
         
-        if (!isDirExists(path)) {
-            createDirectory(path);
-        }
-        if (!isDirExists(server_folder_path) && ip != undefined) {
-            createDirectory(server_folder_path);
-        }
-        console.log(`Writing new report on ${ip} to ${report_path}`)
-        await writeSmth(report_path, text);
+        createDirIfNotExists(path)
+        createDirIfNotExists(paths.server_folder_path)
+
+        console.log(`Writing new report on ${ip} to ${paths.report_path}`)
+        await writeSmth(paths.report_path, text);
     }
 }
