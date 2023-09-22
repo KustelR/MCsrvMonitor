7a84f624ec7fa71303bf0932e6b43b3b16379f1f
KustelR <dmitriysstudios2@gmail.com>
2023-09-22 13:03:09 +0400

    Player system fix, deleted unneccessary data fields, full compatibility with old format, rewriting empty files (Still don't know why they was here)

--- MCsrvMonitor/src/playerSystem/player.js
+++ MCsrvMonitor/src/playerSystem/player.js
@@ -1,23 +1,9 @@
 class Player {
-    constructor(id, nickname, faction, activity_time, last_login, last_activity_check_date, login_count) {
-        this.id = id;
+    constructor(nickname, faction, activity_time) {
         this.nickname = nickname;
         this.faction = faction;
         this.activity_time = activity_time;
-        this.last_login = last_login;
-        this.last_activity_check_date = last_activity_check_date;
-        this.login_count = login_count;
     }
-
-    static get lastId() {
-        return Player.lastId;
-    }
-
-    static set lastId(new_id) {
-        Player.lastId = new_id;
-    }
-
-    static lastId = 0;
 
     setFaction(faction) {
         this.faction = faction;
@@ -25,15 +11,9 @@ class Player {
 
     updateActivityData() {
         let now = Date.now();
-        if (this.activity_time == null) this.activity_time = [now]
-        else this.activity_time.push(now);
+        
+        this.activity_time.push(now);
         this.last_activity_check_date = now;
-
-        this.last_login = now;
-        if (now - this.last_activity_check_date > 24 * 3600 * 1000) {
-            if (!this.login_count) { this.login_count = 1; }
-            this.login_count++;
-        }
     }
 
     getActivityTime() {
@@ -41,16 +21,11 @@ class Player {
     }
 
     toString() {
-        return `${this.nickname} usually active on ${this.activity_time}, plays in ${this.faction}. Last login was ${this.last_login}`;
+        return `${this.nickname} last active on ${this.activity_time[-1]}, plays in ${this.faction}.`;
     }
-
-    id = -1;
     nickname;
     activity_time = [];
     faction;
-    last_login;
-    login_count = 0;
-    last_activity_check_date;
 }
 
 
--- MCsrvMonitor/src/playerSystem/playerReportOperator.js
+++ MCsrvMonitor/src/playerSystem/playerReportOperator.js
@@ -12,16 +12,11 @@ function generatePath(server_ip, path = app_folder_path) {
     server_ip = server_ip.replaceAll('.', '');
     const server_folder_path = `${path}${server_ip}`
     const player_report_folder_path = `${server_folder_path}/playerReports/`
+
+    writer.createDirIfNotExists(server_folder_path);
+    writer.createDirIfNotExists(player_report_folder_path);
     
     return player_report_folder_path
-}
-
-async function getLastId(server_folder_path) {
-    lastIdPath = `${server_folder_path}lastId.txt`;
-    await writer.createDirIfNotExists(server_folder_path);
-    if (!fs.existsSync(lastIdPath)) fs.writeFileSync(lastIdPath, '0');
-    data = fs.readFileSync(lastIdPath, 'utf8');
-    return data;
 }
 
 async function writePlayerReport(player, report_file_path) {
@@ -30,39 +25,38 @@ async function writePlayerReport(player, report_file_path) {
     })
 }
 
+function readPlayerDataJson(player_file_path) {
+    const player_source = fs.readFileSync(player_file_path, 'utf8');
+    if (player_source == "") return null;
+    playerDataJson = JSON.parse(player_source);
+    
+    return PlayerBuilder.BuildFromJson(playerDataJson);
+}
+
+
 function loadPlayerData(server_ip, player_name) {
     report_folder_path = generatePath(server_ip);
 
     const player_file_path = `${report_folder_path}${player_name}.json`;
     if (fs.existsSync(player_file_path)) {
-        playerDataJson = JSON.parse(fs.readFileSync(player_file_path, 'utf8'));
-        return PlayerBuilder.BuildFromJson(playerDataJson);
+        return readPlayerDataJson(player_file_path);
     }
     else return null;
-
 }
 
-function updatePlayerData(server_ip, player_name, last_id) {
+function updatePlayerData(server_ip, player_name) {
     const name = player_name;
-    let isUniquePlayer = false;
 
     let player_data = loadPlayerData(server_ip, name);
     if (player_data === null) {
-        player_data = PlayerBuilder.Build(last_id, name);
-        isUniquePlayer = true;
+        player_data = PlayerBuilder.Build(name);
 
-        console.log(`Unique player detected, id ${last_id}`)
-        if (isUniquePlayer) {
-            last_id++;
-            fs.writeFileSync(lastIdPath, last_id.toString());
-        }
+        console.log(`Unique player detected, id ${player_name}`)
         
     }
     player_data.updateActivityData();
 
     writePlayerReport(player_data, `${server_folder_path}${name}`)
-
-    return isUniquePlayer;
 }
 
 
@@ -72,11 +66,9 @@ async function StorePlayerStatistics(raw_players, server_ip) {
 
         server_folder_path = generatePath(server_ip);
 
-        let last_id = await getLastId(server_folder_path);
-
         current_players_names = raw_players.map((x) => x.name);
     for (let i = 0; i < current_players_names.length; i++) {
-        updatePlayerData(server_ip, current_players_names[i], last_id)
+        updatePlayerData(server_ip, current_players_names[i])
     }
         
     }
